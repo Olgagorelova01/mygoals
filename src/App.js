e0cdf9067a79e091c5a001a195d4d145af28180e
Olgagorelova01 <87213932+Olgagorelova01@users.noreply.github.com>
2022-10-07 20:38:21 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { data} from "./data";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [goal, setGoal] = useState(0);
+    const {id, name, description, source, image} = data[goal];
+    const [showMore, setShowMore] = useState(false);
+
+  
+    const previousGoal = () => {
+        setGoal((goal => {
+            goal --;
+            if (goal < 0) {
+                return data.length - 1;
+            }
+            return goal;
+        }))
+    }
+
+    const nextGoal = () => {
+        setGoal ((goal => {
+            goal ++;
+            if (goal > data.length - 1) {
+                goal = 0;
+            }
+            return goal;
+        }))
+    }
+
+    return(
+        <div>
+            <div className="container">
+                <h1>{name}</h1>
+            </div>
+            <div className="container">
+                <img src={image} width="400px" alt="goals"/>
+            </div>
+            <div className="container">
+                <p>{showMore ? description : description.substring(0, 130) + " ..."}
+                <button className="showMore" onClick={() => setShowMore(!showMore)}>{showMore ? "Show Less" : "Show More"}</button>
+                </p>
+            </div>
+            <div className="container">
+                <button>{source}</button>
+            </div>
+            <div className="btn container">
+                <button onClick={previousGoal}>Previous</button>
+                <button onClick={nextGoal}>Next</button>
+            </div>
+        </div>
+    )
+
 }
 
 export default App;
